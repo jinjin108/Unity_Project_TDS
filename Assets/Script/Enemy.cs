0d4ecf7856e7c1b7104a35acfe4ae7fb78cd5c3b
jinjin108 <maroon1566@gmail.com>
2025-03-11 18:40:12 +0900

    적 점프 기능추가, 몬스터 쌓이는 구조 추가,같은 줄 몬스터만 쌓이기 완성

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,32 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class Enemy : MonoBehaviour
 {
-    Vector3 TargetPosition;
-    float speed = 2f;
-    private Rigidbody2D rb;
+    [SerializeField] float m_Speed = 2;
+
+    [SerializeField] float m_JumpForce;
+
+    [SerializeField] float m_DetectDistance;
+
+    [SerializeField] LayerMask m_EnemyLayerMask;
+
+    private bool m_IsJumping = false;
+    private float m_JumpCooldown = 3f;
+    private float m_LastJumpTime = -3f;
+    BoxCollider2D m_Cl;
+    private Rigidbody2D m_Rb;
+
+    Vector3 m_TargetPosition;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        m_Rb = GetComponent<Rigidbody2D>();
+        m_Cl = GetComponent<BoxCollider2D>();
     }
+
+    private void OnEnable()
+    {
+        m_EnemyLayerMask = LayerMask.GetMask(LayerMask.LayerToName(gameObject.layer));
+    }
+
     public void StartMove(Vector3 startPos)
     {
         transform.position = startPos;
-        TargetPosition = ObjectManager.Instance.TargetPosition.position;
+        m_TargetPosition = ObjectManager.Instance.TargetPosition.position;
         StartCoroutine(MoveToTarget());
     }
 
     IEnumerator MoveToTarget()
     {
-        while (Vector3.Distance(transform.position, TargetPosition) > 0.1f)
+        while (Vector3.Distance(transform.position, m_TargetPosition) > 0.1f)
         {
-            transform.position = Vector3.MoveTowards(transform.position, TargetPosition, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, m_TargetPosition, m_Speed * Time.deltaTime);
+            DetectAndJump();
             yield return null;
+        }
+    }
+
+    void DetectAndJump()
+    {
+        if (Time.time - m_LastJumpTime < m_JumpCooldown || m_IsJumping) return;
+
+        Vector2 rayOrigin = new Vector2(m_Cl.bounds.min.x - 0.1f, transform.position.y + 0.5f);
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.left, m_DetectDistance, m_EnemyLayerMask);
+
+        if (hit.collider != null && hit.collider.gameObject != gameObject)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        m_Rb.AddForce(Vector2.up * m_JumpForce, ForceMode2D.Impulse);
+        m_IsJumping = true;
+        m_LastJumpTime = Time.time;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == gameObject.layer)
+        {
+            m_IsJumping = false;
         }
     }
 }
--- Assets/Script/ObjectManager.cs
+++ Assets/Script/ObjectManager.cs
@@ -7,24 +7,25 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 public class ObjectManager : MonoBehaviour
 {
     public static ObjectManager Instance { get; private set; }
-    [SerializeField]
-    AssetReferenceGameObject EnemyObj;
 
     [SerializeField]
-    Transform SetEnemyPosition1;
+    AssetReferenceGameObject m_EnemyObj;
+
     [SerializeField]
-    Transform SetEnemyPosition2;
+    Transform m_SetEnemyPosition1;
+    [SerializeField]
+    Transform m_SetEnemyPosition2;
 
     [SerializeField]
     public Transform TargetPosition;
 
-    [SerializeField] int PoolSize = 10;
+    [SerializeField] int m_PoolSize = 100;
 
-    List<Enemy> EnemyList = new List<Enemy>();
+    List<Enemy> m_EnemyList = new List<Enemy>();
 
-    float spawnCooldown = 3f;
-    float lastSpawnTime = 0f;
-    int loadedCount = 0;
+    float m_SpawnCooldown = 3f;
+    float m_LastSpawnTime = 0f;
+    int m_LoadedCount = 0;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -43,26 +44,26 @@ public class ObjectManager : MonoBehaviour
 
     void Update()
     {
-        if (Time.time - lastSpawnTime >= spawnCooldown)
+        if (Time.time - m_LastSpawnTime >= m_SpawnCooldown)
         {
             ActivateRandomEnemy();
-            lastSpawnTime = Time.time;
+            m_LastSpawnTime = Time.time;
         }
     }
 
     void InitObject()
     {
-        for (int i = 0; i < PoolSize; i++)
+        for (int i = 0; i < m_PoolSize; i++)
         {
-            EnemyObj.InstantiateAsync(SetEnemyPosition1).Completed += obj =>
+            m_EnemyObj.InstantiateAsync(m_SetEnemyPosition1).Completed += obj =>
             {
                 if (obj.Status == AsyncOperationStatus.Succeeded)
                 {
                     GameObject enemyObj = obj.Result;
                     Enemy enemy = enemyObj.GetComponent<Enemy>();
                     enemyObj.SetActive(false);
-                    EnemyList.Add(enemy);
-                    loadedCount++;
+                    m_EnemyList.Add(enemy);
+                    m_LoadedCount++;
                 }
                 else
                 {
@@ -81,12 +82,12 @@ public class ObjectManager : MonoBehaviour
             Transform spawnPosition;
             if (Random.Range(0, 2) == 0)
             {
-                spawnPosition = SetEnemyPosition1;
+                spawnPosition = m_SetEnemyPosition1;
                 randomEnemy.gameObject.layer = LayerMask.NameToLayer("Enemy1");
             }
             else
             {
-                spawnPosition = SetEnemyPosition2;
+                spawnPosition = m_SetEnemyPosition2;
                 randomEnemy.gameObject.layer = LayerMask.NameToLayer("Enemy2");
             }
 
@@ -97,7 +98,7 @@ public class ObjectManager : MonoBehaviour
 
     Enemy GetRandomInactiveEnemy()
     {
-        foreach (var enemy in EnemyList)
+        foreach (var enemy in m_EnemyList)
         {
             if (!enemy.gameObject.activeInHierarchy)
             {
